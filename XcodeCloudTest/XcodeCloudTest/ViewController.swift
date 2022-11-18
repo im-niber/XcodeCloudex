267194a0d9e62df2330495c6872e0df122e021e9
rbwo552 <rbwo552@naver.com>
2022-11-18 12:12:33 +0900

    [Create] 프로젝트 생성

---
+++ XcodeCloudTest/XcodeCloudTest/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  XcodeCloudTest
+//
+//  Created by rbwo on 2022/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hi")
+    }
+}
